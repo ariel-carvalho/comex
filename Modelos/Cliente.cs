8586f491e280a61237ebe7dd72fcaaf7720b11ef
Ariel Carvalho <ariell_carvalho@hotmail.com>
2024-05-27 20:17:56 -0300

    2 - Organizar classes em pastas e namespaces

---
+++ Modelos/Cliente.cs
@@ -0,0 +1,26 @@
+﻿namespace comex.Modelos;
+
+internal class Cliente
+{
+    public string Nome { get; set; }
+    public string Cpf { get; set; }
+    public string Email { get; set; }
+    public string Profissao { get; set; }
+    public string Telefone { get; set; }
+    public Endereco Endereco { get; set; }
+
+    public void MostraCliente()
+    {
+        Console.WriteLine("Nome: " + Nome);
+        Console.WriteLine("CPF: " + Cpf);
+        Console.WriteLine("E-mail: " + Email);
+        Console.WriteLine("Profissão: " + Profissao);
+        Console.WriteLine("Telefone: " + Telefone);
+        Console.WriteLine("Bairro: " + Endereco.Bairro);
+        Console.WriteLine("Cidade: " + Endereco.Cidade);
+        Console.WriteLine("Complemento: " + Endereco.Complemento);
+        Console.WriteLine("Estado: " + Endereco.Estado);
+        Console.WriteLine("Rua: " + Endereco.Rua);
+        Console.WriteLine("Número: " + Endereco.Numero);
+    }
+}
---
+++ Modelos/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace comex.Modelos;
+
+internal class Produto
+{
+    public string Nome { get; }
+
+    public static List<string> produtos = new List<string>();
+
+    public Produto(string nome)
+    {
+        Nome = nome;
+    }    
+}
